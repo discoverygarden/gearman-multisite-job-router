3f25c05851585f89260da316694dcac759f9d97c
qadan <daitken@discoverygarden.ca>
2017-09-27 17:50:29 -0300

    this one

--- bin/gearman-multisite-job-router.py
+++ bin/gearman-multisite-job-router.py
@@ -4,11 +4,13 @@ import json
 import logging
 import sys
 from argparse import ArgumentParser
-from subprocess import Popen
+from subprocess import Popen, PIPE
 
+# Establish our logger for severe cases.
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%s', level=logging.INFO)
 logger = logging.getLogger('islandora-job-multisite-router')
 
+# Arguments.
 parser = ArgumentParser(
     description='Routes a multisite payload from STDIN to the drush islandora_job router.',
     epilog='The exit code will be passed through from drush if the router command successfully ran. In the case we failed to get that far, exits with -1'
@@ -19,18 +21,21 @@ parser.add_argument('--root', default='/var/www/drupal7', help='Path to the Drup
 if __name__ == '__main__':
     args = parser.parse_args()
 
+    # Get us some JSON from STDIN.
     try:
-        payload = json.loads(sys.stdin.read())
-        print(payload)
+        payload_json = sys.stdin.read()
+        payload = json.loads(payload_json)
     except Exception as e:
         logger.error('Failed to load the payload from STDIN as JSON: {0}'.format(e.message))
         exit(-1)
 
+    # Some validation of the payload.
     for param in ['func', 'uid', 'args', 'site']:
         if param not in payload:
             logger.error('Missing {0} from payload'.format(param))
             exit(-1)
 
+    # Build and run the Drush command.
     drush_args = [
         args.drush,
         '--root={0}'.format(args.root),
@@ -38,7 +43,7 @@ if __name__ == '__main__':
         '--uri={0}'.format(payload['site']),
         'islandora-job-router',
     ]
-
-    drush = Popen(drush_args, stdin=sys.stdin)
-    drush.communicate()
+    drush = Popen(drush_args, stdin=PIPE)
+    drush.communicate(input=payload_json)
+    # Pass along the exit code.
     exit(drush.returncode)
